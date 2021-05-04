8b85f64323efef2c9d20a483aa0d664e41984903
Stenver Suurkütt <stenver.ss@gmail.com>
2021-05-04 11:27:46 +0300

    added comments in code

--- cybercube.js
+++ cybercube.js
@@ -1,6 +1,9 @@
 function findExtraLetter(s, t) {
+    //creating a copy of string t, to remove letters found in string s
     let ans = t  
+    //iterating through every letter in string t
     for (let i = 0; i < t.length; i++) {
+        //checking if letter is included in string s, removing the letter from s and ans if it does
         if ((s.includes(t[i])) && (ans.length > 1)) {
             s = s.replace(t[i], "")
             ans = ans.slice(1)
@@ -10,6 +13,8 @@ function findExtraLetter(s, t) {
     }
     return ans
 }
+
+//running a few different test cases to verify function working properly
 
 console.log(findExtraLetter("abcd", "abcde"))
 console.log(findExtraLetter("abcd", "abecd"))
