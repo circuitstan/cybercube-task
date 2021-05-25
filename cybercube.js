55b0443fa7ca870c4d9d17b32762228a0741f76f
Stenver Suurkütt <stenver.ss@gmail.com>
2021-05-25 15:36:46 +0300

    found the working solution to the closest integer problem

--- cybercube.js
+++ cybercube.js
@@ -22,3 +22,32 @@ console.log(findExtraLetter("abcd", "abbcd"))
 console.log(findExtraLetter("abcd", "abcdb"))
 console.log(findExtraLetter("cdab", "badcf"))
 console.log(findExtraLetter("cdab", "bafdc"))
+
+function findMax(intList) {
+    return Math.max(intList)
+}
+
+let myArray = [1,2,4,7,5,9,10,15,13,19,25]
+
+
+function findClosest(arr, m) {
+    let ans = arr[0]
+    let diff = Math.abs(m - arr[0])
+    for (let i = 1; i < arr.length; i++) {
+        let newdiff = Math.abs(m - arr[i])
+        if (newdiff < diff) {
+            diff = newdiff
+            ans = arr[i]
+        }
+    }
+    return ans
+}
+
+console.log(findClosest(myArray, 11))
+//console shows 10
+console.log(findClosest(myArray, 6))
+//console shows 7
+
+
+
+
